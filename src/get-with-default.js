988f0fc3090878741ef7ac96659b2e44fec0638b
Daniel Narey <danielnarey@users.noreply.github.com>
2019-09-11 22:04:50 -0400

    sync

--- src/get-with-default.js
+++ src/get-with-default.js
@@ -1,17 +1,17 @@
-/* ### `getWithDefault(rec, k, default, [test]) => v|default`
+/* ### `getWithDefault(rec, k, d, [test]) => v|d`
  * If *k* exists in *rec* and passing its associated value *v* to *test*
  * returns `true`, `getWithDefault` returns *v*. If *k* does not exist or 
- * *test* returns `false`, `getWithDefault` returns *default*. If a test 
- * function is not supplied, the default test returns `true` for any value 
- * that is not `undefined` or `null`.
+ * *test* returns `false`, `getWithDefault` returns *d*. If a test function
+ * is not supplied, the default test returns `true` for any value that is not 
+ * `undefined` or `null`.
 */
 export default (
   rec,
   k,
-  default,
+  d,
   test = (x) => (x !== undefined && x !== null),
 ) => rec((obj) => {
   const v = obj[k];
 
-  return test(v) ? v : default;
+  return test(v) ? v : d;
 });
