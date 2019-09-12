b5428c0f6d80e1afb9e2dd8075ab9c304a79bc26
Daniel Narey <danielnarey@users.noreply.github.com>
2019-09-11 23:16:39 -0400

    sync

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'index.js',
+    external: [],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ]
+  },
+];
